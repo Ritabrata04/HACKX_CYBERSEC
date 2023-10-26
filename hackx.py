619a7b41c8869395a3da3621cf63c224bf532940
Danush Khanna <113158564+danushkhanna@users.noreply.github.com>
2023-10-26 03:47:09 -0700

    Update hackx.py

--- hackx.py
+++ hackx.py
@@ -21,7 +21,7 @@ st.image(image, width=200)
 st.markdown(
     "<div style='display: flex; align-items: center; margin-bottom: -35px;'>"
     "<h1 style='color:#003E7F; margin-center: 10px;'> yoU</h1>"
-    "<h1 style='color:F862FC; margin-center: 10px;'> aRe</h1>"
+    "<h1 style='color:#F862FC; margin-center: 10px;'> aRe</h1>"
     "<h1 style='color:#003E7F; margin-center: 10px;'>reaL</h1>",
     unsafe_allow_html=True
 )
