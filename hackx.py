a1ff70c4b696228dd3e7d08d61d62b20a0c60194
Ritabrata Chakraborty <125497227+Ritabrata04@users.noreply.github.com>
2023-10-26 14:05:02 +0530

    Update hackx.py

--- hackx.py
+++ hackx.py
@@ -15,7 +15,7 @@ import pandas as pd
 from extract_features import ExtractFeatures
 from PIL import Image
 
-image = Image.open('/content/muj hackx logo.jpeg')
+image = Image.open('muj hackx logo.jpeg')
 st.image(image, width=150)
 
 st.markdown(
