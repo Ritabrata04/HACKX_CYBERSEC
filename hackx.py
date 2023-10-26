fcecb95b15de27c67a46ebbdcb6482bdb61f22f1
Danush Khanna <113158564+danushkhanna@users.noreply.github.com>
2023-10-26 04:54:27 -0700

    Update hackx.py

--- hackx.py
+++ hackx.py
@@ -24,7 +24,7 @@ from PIL import Image
 image2 = Image.open('muj logo.png')
 image = Image.open('hackxlogowhite.png')
 
-col1, col2, col3 = st.columns([0.5, 3, 3])
+col1, col2, col3 = st.columns([0.1, 5, 3])
 
 with col2:
     st.image(image2, width=200)
